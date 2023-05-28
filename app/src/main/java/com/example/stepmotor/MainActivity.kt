c4831aafa492ab3cef23888cef3dc348facc3ed5
Ivan <zorromen@mail.ru>
2023-05-28 09:18:17 +0300

    5

--- app/src/main/java/com/example/stepmotor/MainActivity.kt
+++ app/src/main/java/com/example/stepmotor/MainActivity.kt
@@ -23,7 +23,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.stepmotor.screen.home.Home
 import com.example.stepmotor.ui.theme.StepMotorwTheme
 import com.google.accompanist.navigation.animation.AnimatedNavHost
